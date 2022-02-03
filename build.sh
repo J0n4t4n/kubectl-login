c61c84929226b581115388b3f1ec947840c356e0
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2022-02-03 16:05:47 +0100

    Set CGO_ENABLED=1 for macOS build to fix DNS issues
    DNS resolution for custom resolver in /etc/resolver aren't respected
    if CGO_ENABLED=0, thus it is set to 1.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export VERSION="0.0.7"
 rm -rf target
 mkdir -p target
 
-env GOOS=darwin GOARCH=amd64 go build -o ./target/oulogin-$VERSION-macos ./kubectl-login.go 
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ./target/oulogin-$VERSION-macos ./kubectl-login.go
 env GOOS=linux GOARCH=amd64 go build -o ./target/oulogin-$VERSION-linux ./kubectl-login.go
 env GOOS=windows GOARCH=amd64 go build -o ./target/oulogin-$VERSION-win.exe ./kubectl-login.go
 
